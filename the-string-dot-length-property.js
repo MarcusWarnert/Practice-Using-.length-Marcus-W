3c1ec1b5ae768da9563bc490038c922cb38d9aa4
MarcusWarnert <mrvwarnert@gmail.com>
2025-07-14 16:44:37 -0400

    Add files via upload

---
+++ the-string-dot-length-property.js
@@ -0,0 +1,14 @@
+let stringOne = "Coding Bootcamp";
+let stringTwo = "JavaScript";
+let stringThree = "";
+
+console.log(stringOne + "has " + stringOne.length " characters.");
+console.log(stringTwo + "has " + stringTwo.length " characters.");
+console.log(stringThree + "has " + stringThree.length " characters.");
+
+let lengthOne;   
+let lengthTwo;   
+let lengthThree; 
+
+
+
